2d6d2b483ada1c44b7956bd59f45548df385eae3
vijayelectem <vijay@electems.com>
2021-09-14 17:15:36 +0530

    post request in elastic search by passing rackId,trayId

--- back-end/app/controllers/elasticsearch.controller.js
+++ back-end/app/controllers/elasticsearch.controller.js
@@ -5,27 +5,49 @@ const app = express()
 app.use(bodyParser.json())
 
 exports.createIndex = (req, res) => {
-  
-  var formAttributesFiltered = filterAttributesBasedOnIsearchable(req.body.attributes);
-  
-    var myBody = '{"content":' + JSON.stringify(formAttributesFiltered)+ '}';
-    var index = req.body.name;
-    var type=req.body.name;
-    var routing = req.body.username;
-    var id=req.body.rackId;
-    client.index({
-      index: index,
-      body: myBody,
-      id:id,
-      type:type,
-      routing:routing
-    },function(err,resp,status) {
-        console.log(resp);
-    });
+  var formAttributesFiltered = JSON.stringify(Object.assign({}, filterAttributesBasedOnIsearchable(req.body.attributes)));
+  const elasticSearchAttributes = {
+    "attributes": formAttributesFiltered,
+     "trayId": JSON.parse(req.body.trayId),
+     "rackId": JSON.parse(req.body.rackId)
   };
+  var myBody = JSON.stringify(elasticSearchAttributes);
+  var index = req.body.name;
+  var type = req.body.name;
+  var routing = req.body.username;
+  client.index({
+    index: index,
+    body: myBody,
+    id: req.body.rackId,
+    type: type,
+    routing: routing
+  }, function (err, resp, status) {
+    console.log(resp);
+  });
+};
+
+app.get("/living", (req, res) => {
+  const searchText = req.query.searchString
+  client.search({
+      index: "living",
+      body: {
+          query: {
+              match: {"name": searchText.trim()}
+          }
+      }
+  })
+  .then(response => {
+      return res.json(response)
+  })
+  .catch(err => {
+      return res.status(500).json({"message": "Error"})
+  })
+})
+
+
 
   function filterAttributesBasedOnIsearchable(formAttributes) {
-    var resultantAttributes=[{}];
+    var resultantAttributes=[];
     let filteredObjects = formAttributes.filter(attributes =>attributes.isSearchable == 'true');
    for (let i = 0; i < filteredObjects.length; i++) {
    resultantAttributes[i]=filteredObjects[i].label+":"+filteredObjects[i].value;
