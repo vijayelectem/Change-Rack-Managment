454e3a2313e975620d00cba9feec00c15d316f4c
vijayelectem <vijay@electems.com>
2021-09-15 19:33:10 +0530

    code commit for multiselect drop down for selecting stores while creating Rack

--- Front-end/src/app/components/create-rack/create-rack.component.ts
+++ Front-end/src/app/components/create-rack/create-rack.component.ts
@@ -23,7 +23,7 @@ export class CreateRackComponent implements OnInit {
     no_of_columns:0,
     client_fk:0,
     createdon:'',
-    store_fk:0,
+    stores:{}
     
   };
   store: Store={
@@ -44,9 +44,19 @@ export class CreateRackComponent implements OnInit {
   rackForm: FormGroup;
   rackObj:any;
   createdon:any;
+  dropdownSettings = {};
   isStorePresent=true;
-
+  categories :any = [];
+  selected:any = [];
   ngOnInit() {
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'storeId',
+      textField: 'storeName',
+      selectAllText: 'Select All',
+      unSelectAllText: 'Unselect All',
+      itemsShowLimit: 3
+   }
     this.PlanObj = JSON.parse(sessionStorage.getItem('planObj'));
     this.UserObj = JSON.parse(sessionStorage.getItem('userObj'));
      this.rack.client_fk = this.UserObj.clientFk;
@@ -57,6 +67,7 @@ export class CreateRackComponent implements OnInit {
       createdon:['', [Validators.required]],
       client_fk:this.UserObj.clientFk,
       storeFk:null,
+      createdBy:'',
   });
 
   if(this.PlanObj[0].name == 'Personal'){
@@ -66,10 +77,20 @@ export class CreateRackComponent implements OnInit {
   this.getStores(this.UserObj.clientFk);
   }
 
+  onItemSelect(item: any) {
+    this.selected.push(item);
+  }
+  onSelectAll(items: any) {
+    this.selected.push(items);
+  }
+  getSelectedValue(){
+    console.log(this.selected);
+  }
+
   getStores(client_fk): void{
     this.storeService.fetchAllStoresByClientFK(client_fk)
     .subscribe((data: any) => {
-     this.storeList= data;
+     this.categories = data;
     },
       error => {
         console.log(error);
@@ -78,12 +99,13 @@ export class CreateRackComponent implements OnInit {
 
   get f() { return this.rackForm.controls; }
 
-  saveRack(): void {
+  saveRack(data:any): void {
+    data.stores = this.selected;
     this.submitted = true;
     if (this.rackForm.invalid) {
       return;
     }
-    this.rackService.createRack(this.rackForm.value)
+    this.rackService.createRack(data)
       .subscribe(
         response => {
           console.log(response);
